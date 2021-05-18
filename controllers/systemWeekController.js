5711fd09ede43444b5ebd326a75d388ad2f7cff1
Tiffani Hess <hesstjune@gmail.com>
2021-05-18 11:54:07 -0500

    Throw custom error at systemWeekController startNewWeek if date not >= today

--- controllers/systemWeekController.js
+++ controllers/systemWeekController.js
@@ -1,6 +1,7 @@
 const { SystemWeek, System } = require("../db");
 const { Op } = require("sequelize");
 const moment = require("moment");
+const { CustomError } = require("../handlers/errorHandlers");
 
 exports.getCurrentWeek = async (req, res) => {
   const { id: userId } = req.token.data;
@@ -20,18 +21,25 @@ exports.getCurrentWeek = async (req, res) => {
 exports.setNewWeek = async (req, res) => {
   const { id: userId } = req.token.data;
   const { startDate } = req.body;
-  const endDate = moment(startDate).add(6, "days").format("YYYY-MM-DD");
+  const validDate = moment(startDate).isSameOrAfter(moment());
 
-  const record = await SystemWeek.create({ startDate, endDate, userId });
+  // Safety measure - check if start date >= today before creating record
+  if (validDate) {
+    const endDate = moment(startDate).add(6, "days").format("YYYY-MM-DD");
 
-  res.status(201).json({ data: record });
+    const record = await SystemWeek.create({ startDate, endDate, userId });
+
+    res.status(201).json({ data: record });
+  } else {
+    throw new CustomError("system.invalidDate", "SystemWeekError", 400);
+  }
 };
 
 exports.deleteCurrentWeek = async (req, res) => {
   const id = req.params.id;
   const { id: userId } = req.token.data;
 
-  //delete system records associated with week
+  //delete user's system records associated with week
   const deletedSystemRecords = await System.update(
     { isDeleted: true },
     { where: { [Op.and]: [{ systemWeekId: id }, { userId }] } }
