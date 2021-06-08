007b46368a02ba347706cdc3359e0c5b93f5d1d5
Tiffani Hess <hesstjune@gmail.com>
2021-06-08 13:49:48 -0500

    Yet again refactoring password reset url

--- config/passwordResetConfig.js
+++ config/passwordResetConfig.js
@@ -3,7 +3,7 @@ let resetPasswordUrl;
 if (process.env.NODE_ENV === "development") {
   resetPasswordUrl = "http://localhost:3000/reset-password";
 } else {
-  resetPasswordUrl = "https://four-fs-server.herokuapp.com/reset-password";
+  resetPasswordUrl = "https://four-fs.herokuapp.com/reset-password";
 }
 
 exports.RESET_PASSWORD_URL = resetPasswordUrl;
