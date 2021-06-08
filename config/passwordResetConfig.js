e37ef95e59969d2cec01ab2f4b8f354af23639de
Tiffani Hess <hesstjune@gmail.com>
2021-06-08 13:36:09 -0500

    Added prod url for password reset

--- config/passwordResetConfig.js
+++ config/passwordResetConfig.js
@@ -1,10 +1,9 @@
 let resetPasswordUrl;
 
-// if (process.env.NODE_ENV === "development") {
-//   resetPasswordUrl = "http://localhost:3000/reset-password";
-// } else {
-//   resetPasswordUrl = "https://4fsmemberportal.herokuapp.com";
-// }
-resetPasswordUrl = "http://localhost:3000/reset-password";
+if (process.env.NODE_ENV === "development") {
+  resetPasswordUrl = "http://localhost:3000/reset-password";
+} else {
+  resetPasswordUrl = "https://four-fs-server.herokuapp.com";
+}
 
 exports.RESET_PASSWORD_URL = resetPasswordUrl;
