b6eb895d3ddd0301ff8175a3b141c6c31241a885
Tiffani Hess <hesstjune@gmail.com>
2021-06-24 22:18:03 -0500

    SendGrid config

--- handlers/emailHandler.js
+++ handlers/emailHandler.js
@@ -2,13 +2,15 @@ const nodemailer = require("nodemailer");
 const ejs = require("ejs");
 const juice = require("juice");
 const { CustomError } = require("../handlers/errorHandlers");
+const sgMail = require("@sendgrid/mail");
 
 const transport = nodemailer.createTransport({
-  host: process.env.MAILTRAP_HOST,
-  port: process.env.MAILTRAP_PORT,
+  service: "SendGrid",
+  host: process.env.SENDGRID_HOST,
+  port: process.env.SENDGRID_PORT,
   auth: {
-    user: process.env.MAILTRAP_USER,
-    pass: process.env.MAILTRAP_PASS,
+    user: process.env.SENDGRID_USER,
+    pass: process.env.API_KEY,
   },
 });
 
@@ -21,6 +23,8 @@ const generateHTML = async (filename, options) => {
 };
 
 exports.sendEmail = async (options) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
   try {
     const html = await generateHTML(options.filename, options);
 
@@ -31,14 +35,18 @@ exports.sendEmail = async (options) => {
       html,
       attachments: [
         {
+          contentType: "image/png",
           filename: "4flogo.png",
           path: "public/assets/4flogo.png",
+          content: process.env.SENDGRID_IMAGE_B64,
           cid: "logo",
+          disposition: "inline",
+          content_id: "logo",
         },
       ],
     };
 
-    return transport.sendMail(mailOptions);
+    return sgMail.send(mailOptions);
   } catch (err) {
     throw new CustomError(
       "passwordResetEmail.failure",
